dbdd399db1c1537f47670d99d8af6170cd8a0a6f
muftimuf <tiimuf.personal@gmail.com>
2022-07-30 12:48:36 +0700

    fix mobile nav menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -25,12 +25,12 @@ hamburger.addEventListener("click", function () {
 });
 
 // Klik di luar hamburger menu
-window.addEventListener("click", function (e) {
-  if (e.target !== hamburger && e.target !== navMenu) {
-    hamburger.classList.remove("hamburger-active");
-    navMenu.classList.add("hidden");
-  }
-});
+// window.addEventListener("click", function (e) {
+//   if (e.target !== hamburger && e.target !== navMenu) {
+//     hamburger.classList.remove("hamburger-active");
+//     navMenu.classList.add("hidden");
+//   }
+// });
 
 // Darkmode toggle
 const darkToggle = document.querySelector("#dark-toggle");
